64416dddb35363c4327b5826413015ed6bca789d
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2022-12-17 11:03:04 +0530

    Fixed backend api routes

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,6 @@ const port = process.env.PORT || 5000
 
 
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`)
-})
-
 
 app.get("/auth/google/callback",passport.authenticate("google",{
     successRedirect : process.env.CLIENTURL,
@@ -50,12 +46,7 @@ app.get("/auth/login/failed",(req,res)=>{
 app.get("/auth/login/success",(req,res)=>{
     const user = passportStrategy.user
     if(user != null){
-        console.log(user)
-      res.sendStatus(200).json({
-        error:false,
-        message: "sucessfully logged in",
-        user :user,
-      })
+    res.send(user)
     }
     else{
         res.sendStatus(403).json({
@@ -69,6 +60,23 @@ app.get("/auth/google",passport.authenticate("google",['profile','email']))
 
 
 app.get("/auth/logout",(req,res)=>{
-    req.logout();
-    res.redirect(process.env.CLIENTURL)
+    req.logout((err) =>{
+        if(err) {
+            console.log(err)
+        }
+        res.redirect(process.env.CLIENTURL)
+            console.log("Logout Successful")
+        });
+       
+   
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("<a href='/auth/google/callback'> AUth</a>")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
 })
